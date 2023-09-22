d119f305374c75c9155d82c52fb43a28cd63cb9e
Stronauta <samircastillop@gmail.com>
2023-09-21 21:41:24 -0400

    Se agrego el debuggingExtesions. se agregaron importaciones y se trabajo en el diseño de la pagina y en el code de este

---
+++ Clients/ExtDepuracion.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json;
+
+namespace PizzeriaPlace.Clients
+{
+	public static class ExtDepuracion
+	{
+		private static JsonSerializerOptions opti = new JsonSerializerOptions { WriteIndented = true };
+		public static string ToJson(this object obj) => JsonSerializer.Serialize(obj, opti);
+	}
+}
